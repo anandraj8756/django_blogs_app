704fefcdc11cd5537706ace0630c3c15b61501f4
Anand Raj <anandallen95@gmail.com>
2021-06-03 08:58:22 +0530

    first tutorials

---
+++ mysite/mysite/urls.py
@@ -0,0 +1,9 @@
+
+from threading import main_thread
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('main.urls')),
+]
