2c773e508da51552dda05b63b09690f4b4e8ada0
Anand Raj <anandallen95@gmail.com>
2021-06-03 20:41:10 +0530

    admin and app

--- mysite/main/admin.py
+++ mysite/main/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from django.db import models
+from .models import Tutorial
+from tinymce.widgets import TinyMCE
 
 # Register your models here.
+
+class TutorialAdmin(admin.ModelAdmin):
+
+    fieldsets = [
+        ("Title/date", {'fields': ["tutorial_title", "tutorial_published"]}),
+    #    ("URL", {'fields': ["tutorial_slug"]}),
+     #   ("Series", {'fields': ["tutorial_series"]}),
+        ("Content", {"fields": ["tutorial_content"]})
+    ]
+
+    formfield_overrides = {
+        models.TextField: {'widget': TinyMCE()},
+    }
+
+admin.site.register(Tutorial, TutorialAdmin)
